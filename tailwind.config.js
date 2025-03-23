d42170527f4b281226bc96f67d0c3527311bc45c
Kirill Grabar <kirill.grabar4@gmail.com>
2025-03-23 22:47:46 +0100

    feat: implement tab navigation with custom icons and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      primary: '#030014',
+      secondary: '#151312',
+      accent: '#AB8BFF',
+      ligth: {
+        100: '#D6C6FF',
+        200: '#A8B5DB',
+        300: '#9CA4AB',
+      },
+      dark: {},
+    },
   },
   plugins: [],
 };
