bc95ace0431a75c53550937c8584d6ab73aa7283
Kirill Grabar <kirill.grabar4@gmail.com>
2025-03-24 22:42:18 +0100

    feat: index page background and logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Link } from 'expo-router';
+import { images } from '@/constants/images';
+import { icons } from '@/constants/iconst';
 
 const index = () => {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <Text>index</Text>
-      <Link className="text-4xl" href="/onboarding">
-        Link to unboarding
-      </Link>
-    </SafeAreaView>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"></Image>
+      <ScrollView className="flex-1 top-0 px-5">
+        <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5"></Image>
+      </ScrollView>
+    </View>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,20 @@ module.exports = {
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
-      primary: '#030014',
-      secondary: '#151312',
-      accent: '#AB8BFF',
-      ligth: {
-        100: '#D6C6FF',
-        200: '#A8B5DB',
-        300: '#9CA4AB',
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+        light: {
+          100: '#D6C7FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark: {
+          100: '#221F3D',
+          200: '#0F0D23',
+        },
+        accent: '#AB8BFF',
       },
-      dark: {},
     },
   },
   plugins: [],
